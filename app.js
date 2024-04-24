fd62e11c8356799f0635511adb5fedd34fadc380
ivan <ivbl21@yandex.ru>
2024-04-24 20:53:28 +0300

    project init

---
+++ app.js
@@ -0,0 +1,6 @@
+import Obj from "./obj.js";
+export default class {
+    constructor() {
+        Obj.init();
+    }
+}
---
+++ obj.js
@@ -0,0 +1,5 @@
+export default {
+    init() {
+        console.log("Object is initiated!");
+    }
+}
